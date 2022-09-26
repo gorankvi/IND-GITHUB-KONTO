a3c0b7d025732554fa52e13db9762cbd9ec03d99
gorankvi <goran.kvitne@gmail.com>
2022-09-26 16:22:45 +0200

    test

---
+++ Github/TestFlagg.js
@@ -0,0 +1,10 @@
+
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.fillStyle ="#BC002D";
+ctx.arc(300, 200, 120, 0, 2 * Math.PI);
+ctx.fill();
+
+
